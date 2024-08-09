e8c5c32cd9ef4de75217ec04cb1481b1a1d76db3
Guilherme Buys <guibuys55@gmail.com>
2024-08-09 12:11:56 -0300

    second commit

--- quiz_data.py
+++ quiz_data.py
@@ -5,34 +5,29 @@ quiz__data = {
         "answer": "Rússia"
     },
     {
-        "question": 'Quem pintou a obra "Mona Lisa"?',
-        "choices": ["China", "Canadá", "Rússia", "Estados Unidos"],
+        "question": 'Qual é o maior planeta do sistema solar?',
+        "choices": ["Plutão", "Júpiter", "Saturno", "Urano"],
+        "answer": "Júpiter"
+    },
+    {
+        "question": "Quem pintou a Mona Lisa?",
+        "choices": ["Leonardo da Vinci", "Vincent Van Gogh", "Pablo Picasso", "Michelangelo"],
+        "answer": "Leonardo da Vinci"
+    },
+    {
+        "question": "Maior oceano do mundo",
+        "choices": ["Pacífico", "Índico", "Atlântico", "Ártico", "Estados Unidos"],
         "answer": "Rússia"
     },
     {
-        "question": "Qual é o país com a maior extensão territorial?",
-        "choices": ["China", "Canadá", "Rússia", "Estados Unidos"],
-        "answer": "Rússia"
+        "question": "Cu",
+        "choices": ["Ouro", "Platina", "Cobre", "Oxigênio"],
+        "answer": "Cobre"
     },
     {
-        "question": "Qual é o país com a maior extensão territorial?",
-        "choices": ["China", "Canadá", "Rússia", "Estados Unidos"],
-        "answer": "Rússia"
-    },
-    {
-        "question": "Qual é o país com a maior extensão territorial?",
-        "choices": ["China", "Canadá", "Rússia", "Estados Unidos"],
-        "answer": "Rússia"
-    },
-    {
-        "question": "Qual é o país com a maior extensão territorial?",
-        "choices": ["China", "Canadá", "Rússia", "Estados Unidos"],
-        "answer": "Rússia"
-    },
-    {
-        "question": "Qual é o país com a maior extensão territorial?",
-        "choices": ["China", "Canadá", "Rússia", "Estados Unidos"],
-        "answer": "Rússia"
+        "question": "Sal de cozinha",
+        "choices": ["NaOH", "NaCl", "O3", "H2O"],
+        "answer": "NaOH"
     },
     {
         "question": "Qual é o país com a maior extensão territorial?",
